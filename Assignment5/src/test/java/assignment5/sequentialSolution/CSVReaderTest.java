4e074a649c4d02ff9678c61d37df32cd3dddc150
Kabra, Ambika <kabra.am@northeastern.edu>
2023-01-09 11:16:13 -0800

    assignment5

---
+++ Assignment5/src/test/java/assignment5/sequentialSolution/CSVReaderTest.java
@@ -0,0 +1,27 @@
+package assignment5.sequentialSolution;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CSVReaderTest {
+  CSVReader testReader;
+  String basePath = System.getProperty("user.dir");
+  String testFile;
+
+  @BeforeEach
+  void setUp() {
+    testFile = basePath +"/src/test/resources/studentVle.csv";
+    testReader = new CSVReader();
+  }
+
+  @Test
+  void readFile() throws CsvValidationException, IOException {
+    testReader.readFile(testFile);
+    assertEquals(4,testReader.getCsvContent().size());
+  }
+
+}
